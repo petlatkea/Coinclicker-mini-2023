bb724585bbe840ae4d48ac650c7176cfa1dd0795
Peter Lind (@KEA) <petl@kea.dk>
2023-02-17 11:12:10 +0100

    click på coin animation kan gentages

--- script.js
+++ script.js
@@ -3,16 +3,40 @@ window.addEventListener("load", start);
 function start() {
   console.log("JavaScript kører!");
 
+
+
   document.querySelector("#coin1_container").addEventListener("click", clickCoin);
 
 }
 
 function clickCoin() {
   console.log("Click coin");
+  // Forhindr gentagne clicks
+  document.querySelector("#coin1_container").removeEventListener("click", clickCoin);
+  
   // Stop coin container
   document.querySelector("#coin1_container").classList.add("paused");
 
   // sæt forsvind-animation på coin
   document.querySelector("#coin1_sprite").classList.add("zoom_out");
 
+  // når forsvind-animation er færdig: coinGone
+  document.querySelector("#coin1_container").addEventListener("animationend", coinGone);
+  
+}
+
+function coinGone() {
+  // fjern event der bringer os herind
+  document.querySelector("#coin1_container").removeEventListener("animationend", coinGone);
+
+  // fjern forsvind-animation
+  document.querySelector("#coin1_sprite").classList.remove("zoom_out");
+  
+  // fjern pause
+  document.querySelector("#coin1_container").classList.remove("paused");
+
+  // TODO: genstart falling animation
+
+  // gør det muligt at klikke på coin igen
+  document.querySelector("#coin1_container").addEventListener("click", clickCoin);
 }
